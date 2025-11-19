800dd1b15cfabc078f873ed32f7488385f1ba931
taha230 <taha.hamedani8@gmail.com>
2025-11-19 14:55:44 +0330

    add all three models and migrate

--- products/models.py
+++ products/models.py
@@ -9,21 +9,21 @@ class Category(models.Model):
 
     title = models.CharField(_('title') ,max_length=50) #verbose_name show in database
     description = models.TextField(_('description'), blank=True)
-    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories')
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories/')
     is_enable = models.BooleanField(_('is_enable'), default = True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
 
     class Meta:
         db_table = 'categories'
-        verbos_name = _('Category') # show in admin
-        verbos_name_plural = _('Categories')
+        verbose_name = _('Category') # show in admin
+        verbose_name_plural = _('Categories')
 
 
 class Product(models.Model):
     title = models.CharField(_('title'), max_length=50)
     description = models.TextField(_('description'), blank=True)
-    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories')
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='products/')
     is_enable = models.BooleanField(_('is_enable'), default=True)
     categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
@@ -31,20 +31,20 @@ class Product(models.Model):
 
     class Meta:
         db_table = 'products'
-        verbos_name = _('Product') # show in admin
-        verbos_name_plural = _('Products')
+        verbose_name = _('Product') # show in admin
+        verbose_name_plural = _('Products')
 
 
 class File(models.Model):
 
-    product = models.ForeignKey('Product', verbose_name=_('product'), blank=True)
+    product = models.ForeignKey('Product', verbose_name=_('product'), blank=True, on_delete=models.CASCADE)
     title = models.CharField(_('title'), max_length=50)
     is_enable = models.BooleanField(_('is_enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
-    file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
+    file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/') # can be upload to files too
 
     class Meta:
         db_table = 'files'
-        verbos_name = _('File') # show in admin
-        verbos_name_plural = _('Files')
+        verbose_name = _('File') # show in admin
+        verbose_name_plural = _('Files')
